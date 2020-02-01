91ee6a44aef5f665d210d5d6a79227a83ceb009c
Craig Gant <craigkgant@gmail.com>
2020-02-01 14:42:48 -0500

    fixed error with html routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,55 @@ const db = require("../models");
 
 module.exports = function(app) {
  
+  // Get request for all workout documents
   app.get("/api/workouts", (req, res) => {
 
     db.Workout.find({})
-      .then((err, results) => {
-      if (err) throw err;
+      .then(results => {
       res.json(results);
+      console.log(results);
     })
+    .catch(err => {
+      res.json(err);
+    });
+  });
+
+  // Creates a new workout
+  app.post("/api/workouts", ({body}, res) => {
+
+    db.Workout.create(body)
+    .then(results => {
+      res.json(results);
+      console.log(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
   })
 
-  
+  // Updates exercies to the workout
+  app.post("/api/workouts/:id", (req, res) => {
+
+    db.Workout.updateOne({_id: req.params.id})
+    .then(results => {
+      res.json(results);
+      console.log(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  })
+
+  // Get request for last 7 workouts
+  app.get("/api/workouts/range", (req, res) => {
+
+    db.Workout.find({}).sort({_id:-1}).limit(7)
+    .then(results => {
+      res.json(results);
+      console.log(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  })
 };
