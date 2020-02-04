bcf1aa8de15279cb55430d05e1efd8e7a8fd7105
Craig Gant <craigkgant@gmail.com>
2020-02-03 20:36:15 -0500

    refactored db variables

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.MONGODB_URI || 3000;
+const PORT = process.env.PORT || 3000;
 
 // declares variable 'app' for use
 const app = express();
@@ -23,8 +23,8 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-let MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/workout", { useNewUrlParser: true });
-mongoose.connect(MONGODB_URI);
+// let MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`)
